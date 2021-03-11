8b3adfb6be65048da55e77b12cd37877055f92a5
jainendra <jainendraprakash13901@gmail.com>
2021-03-11 09:58:47 +0530

    Completed authentication part

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 
@@ -36,3 +36,14 @@ def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request,'todo/loginuser.html',{'forms':AuthenticationForm()})
+    else :
+        user = authenticate(request,username = request.POST['username'],password = request.POST['password'])
+        if user is None:
+            return render(request,'todo/loginuser.html',{'forms':AuthenticationForm(),'error':'Username and password did not match'})
+        else :
+            login(request,user)
+            return redirect('home')
--- todowoo_project/urls.py
+++ todowoo_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     # Auth
     path('signup/',views.signupuser,name = 'signupuser'),
     path('logout/',views.logoutuser,name = 'logoutuser'),
+    path('login/',views.loginuser,name = 'loginuser'),
     # To do
     path('',views.home,name = 'home'),
     path('current/',views.current,name = 'current'),
